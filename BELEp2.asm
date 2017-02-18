176ca137e4e49f7dc4907e8b6118a20f4d0a8b0d
Bennet Leff <bennetleff@gmail.com>
2017-02-17 20:44:18 -0600

    fixed register names to fit project spec

--- BELEp2.asm
+++ BELEp2.asm
@@ -9,37 +9,37 @@ main:
 	la $a0, buffer	# load byte space into address
 	li $a1, 20	# allot the byte space for string
 	
-	move $t0, $a0	# move the string into $t0
 	syscall
 
 	addi $t1, $t1,   0 	# will be i for the ith element
 	addi $t3, $zero, 1  	# holds the result of 31^i
 	addi $t4, $zero, 31 	# hold the val to multiply $t2 by each iteration
-	addi $t5, $zero, 0	# holds length of string
 	addi $t6, $zero, 0  	# will store the final output
 	addi $t7, $zero, 31 	# only purpose is holding const. 31
 	addi $t8, $zero, 0	# hods s_i * 31^i before it's added to $t6
 	addi $t9, $zero, 0xa  	# val of newline char
 
 checkNull:
-	lb   $t2, 0($t0)
+	lb   $t2, 0($a0)
 	beqz $t2, end	 	# if the first char is 0 or newline, it's empty so
 	beq  $t2, $t9, end	# we end the program, otherwise keep going with it
+	
+	add $a1, $zero, $zero   # zero out a1 so we can use it as "n"
 		
 getLen:
-	lb $t2, 0($t0)
+	lb  $t2, 0($a0)
 	beq $t2, $t9, hashN
 	
-	addi $t0, $t0, 1	# update string index
-	addi $t5, $t5, 1 	# increase len of string
+	addi $a0, $a0, 1	# update string index
+	addi $a1, $a1, 1 	# increase len of string
 	
 	j getLen
 			
-hashN:				# assumes $t5 (str len) has been set
-	lb   $t2, -1($t0)  	# t2 will hold the hex val of char at each interation
+hashN:				# assumes $a1 (str len) has been set
+	lb   $t2, -1($a0)  	# t2 will hold the hex val of char at each interation
 				# starts -1 index back because we read an extra index
 				# for the new line character in getLen
-	beqz $t5, printCalc	# goto end if we run out of chars
+	beqz $a1, printCalc	# goto end if we run out of chars
 
 	mult $t2, $t3		# s_i * (31^i)
 	mflo $t8		# store above calc in $t8
@@ -49,11 +49,11 @@ hashN:				# assumes $t5 (str len) has been set
 	mult $t7, $t3 		# 31 ^ i
 	mflo $t3	
 			
-	addi $t0, $t0, -1	# dec string address to read backwards
-	addi $t5, $t5, -1       # subtract from loop counter
+	addi $a0, $a0, -1	# dec string address to read backwards
+	addi $a1, $a1, -1       # subtract from loop counter
 	j hashN		
 		
-printCalc:	
+printCalc:
 	add $a0, $zero, $t6
 	li $v0, 1
 	syscall
