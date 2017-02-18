143345c67559e64fdd9adc7b4b3d1bab779262b5
Bennet Leff <bennetleff@gmail.com>
2017-02-17 20:31:53 -0600

    added loop everytime calculated

--- BELEp2.asm
+++ BELEp2.asm
@@ -1,4 +1,3 @@
-
 .data 
 	buffer: .space 20
 
@@ -22,6 +21,11 @@ main:
 	addi $t8, $zero, 0	# hods s_i * 31^i before it's added to $t6
 	addi $t9, $zero, 0xa  	# val of newline char
 
+checkNull:
+	lb   $t2, 0($t0)
+	beqz $t2, end	 	# if the first char is 0 or newline, it's empty so
+	beq  $t2, $t9, end	# we end the program, otherwise keep going with it
+		
 getLen:
 	lb $t2, 0($t0)
 	beq $t2, $t9, hashN
@@ -35,7 +39,7 @@ hashN:				# assumes $t5 (str len) has been set
 	lb   $t2, -1($t0)  	# t2 will hold the hex val of char at each interation
 				# starts -1 index back because we read an extra index
 				# for the new line character in getLen
-	beqz $t5, end		# goto end if we run out of chars
+	beqz $t5, printCalc	# goto end if we run out of chars
 
 	mult $t2, $t3		# s_i * (31^i)
 	mflo $t8		# store above calc in $t8
@@ -49,10 +53,18 @@ hashN:				# assumes $t5 (str len) has been set
 	addi $t5, $t5, -1       # subtract from loop counter
 	j hashN		
 		
-end:	
+printCalc:	
 	add $a0, $zero, $t6
 	li $v0, 1
 	syscall
+	
+	# print a newline after printing the calculation
+	addi $a0, $zero, 0xa
+	addi $v0, $zero, 11	 
+	syscall
+	
+	j main
 
+end:
 	li $v0, 10	# end program
 	syscall	
